d3e63ae175fc038505d8719d943538a5b153b74b
Ivo <ivo.h@centrum.cz>
2018-09-22 14:14:17 +0200

    added textEditorcCommand added function to get text selection

--- src/extension.ts
+++ src/extension.ts
@@ -3,22 +3,64 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+
+
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "sendtoscalarepl" is now active!');
+    console.log('Extension "sendtoscalarepl" is now active!');
+
+    function getSelectionText(textEditor: vscode.TextEditor) : string[] {
+        let selection = textEditor.selection;       
+        if (!selection) {
+            console.error(`No selection.`)
+            return [];
+        }
+        
+        if (selection.isEmpty) {
+            console.log(`No selected text, therefore selecting current line.`)
+            return [textEditor.document.lineAt(selection.start.line).text];
+        }
+
+        let result : string[] = [];
+        for (let line = selection.start.line; line <= selection.end.line; line++){
+            let currentLine = textEditor.document.lineAt(line).text;
+            if (line === selection.end.line) {
+                currentLine = currentLine.substring(0, selection.end.character)
+            }
+            if (line === selection.start.line) {
+                currentLine = currentLine.substring(selection.start.character)
+            }
+            result.push(currentLine)
+        }
+        console.log(`Selected text is '${result.length}' rows.`)
+        return result;
+    }
+
+    function sendToTerminal(text: string) {
+        vscode.window.showInformationMessage(`Hello $() World!`);
+    }
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let disposable = vscode.commands.registerTextEditorCommand('extension.sendToScalaREPL', (textEditor: vscode.TextEditor) => {
         // The code you place here will be executed every time your command is executed
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        // vscode.termin
+        let selection = getSelectionText(textEditor)
+        if (selection.length === 0) {
+            return;
+        }
+
+
+        for (let item in selection) {
+            sendToTerminal(item);
+        }        
     });
 
     context.subscriptions.push(disposable);
